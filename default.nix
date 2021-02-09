98df784aa9509e40b06708a488243db0310b64f1
Dan Dart <git@dandart.co.uk>
2021-02-09 21:36:39 +0000

    update nix

--- default.nix
+++ default.nix
@@ -1,2 +1,31 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc884" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./fsutils.nix { }
+{ nixpkgs ? import <nixpkgs> {},
+  compiler ? "ghc8102" }:
+let
+  gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: rec {
+      fsutils = self.callCabal2nix "fsutils" (gitignore ./.) {};
+    };
+  };
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p.fsutils
+    ];
+    buildInputs = [
+      nixpkgs.haskellPackages.cabal-install
+      nixpkgs.wget
+      nixpkgs.haskellPackages.stack
+      nixpkgs.haskellPackages.ghcid
+      nixpkgs.haskellPackages.stylish-haskell
+      nixpkgs.haskellPackages.hlint
+    ];
+    # withHoogle = true;
+  };
+  exe = nixpkgs.haskell.lib.justStaticExecutables (myHaskellPackages.fsutils);
+in
+{
+  inherit shell;
+  inherit exe;
+  inherit myHaskellPackages;
+  fsutils = myHaskellPackages.fsutils;
+}
